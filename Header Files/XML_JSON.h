41e2ae0fad66b37d904de2795f0ea132c25f6465
Moaaz ibrahim <55041428+elBanna00@users.noreply.github.com>
2021-12-23 22:36:23 +0200

    Add files via upload

---
+++ Header Files/XML_JSON.h
@@ -0,0 +1,17 @@
+#ifndef XML_JSON_H_
+#define XML_JSON_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+string fitting(string in);
+string spacescount(int);
+
+void XMLJSON(string );
+
+#endif /* XML_JSON_H_ */
---
+++ Header Files/huffman.h
@@ -0,0 +1,51 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <queue>
+#include <fstream>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<time.h>
+#include<stdlib.h>
+#include <filesystem>
+
+using namespace std;
+#define Char_size 256
+
+struct Node                                                                                     //Structure of a Node of a Huffman tree for decoding
+{
+    long long int Freq;
+    unsigned char character;
+    Node* left;
+    Node* right;
+    Node(char c,Node* l=NULL,Node* r=NULL)
+    {
+        character=c;
+        left=l;
+        right=r;
+    }
+    Node(unsigned char c,long long int f,Node* l=NULL,Node* r=NULL)
+    {
+        character=c;
+        Freq=f;
+        left=l;
+        right=r;
+    }
+
+};
+Node* Make_Huffman_tree(ifstream& );
+void Mindownheap(vector<Node*>&,int ,int );
+Node* Extract_min(vector<Node*>&);
+void Insert_MinHeap(vector<Node*> &,Node* );
+void Build_Minheap(vector<Node*> &,int );
+void store_codes(Node* ,char[],int ,vector<long long int>&) ;
+void store_tree(ofstream &,Node* );
+Node* Huffman(long long int[]);
+void Write_compressed(ifstream&,ofstream&,vector<long long int >&);
+void decode(ifstream&,string ,Node*,long long int);
+void Decompresing(string);
+void compresing(string);
+
+#endif // HUFFMAN_H_INCLUDED
